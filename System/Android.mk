9b55d71148634b88820923ad5ca28e200c150c3b
SKULSHADY <anushekprasal@gmail.com>
2018-10-20 19:03:25 +0530

    Test

--- System/Android.mk
+++ System/Android.mk
@@ -1,9 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := System_Transparent
-LOCAL_CERTIFICATE := platform
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_PACKAGE_NAME := System_Transparent
+LOCAL_MODULE_TAGS := optional
 
-include $(BUILD_RRO_SYSTEM_PACKAGE)
+LOCAL_PACKAGE_NAME := SystemBlackTheme
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+
+include $(BUILD_PACKAGE)
--- SystemUI/Android.mk
+++ SystemUI/Android.mk
@@ -1,9 +1,12 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := SystemUI_Transparent
-LOCAL_CERTIFICATE := platform
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_PACKAGE_NAME := SystemUI_Transparent
+LOCAL_MODULE_TAGS := optional
 
-include $(BUILD_RRO_SYSTEM_PACKAGE)
+LOCAL_PACKAGE_NAME := SystemUIBlackTheme
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+
+include $(BUILD_PACKAGE)
+
